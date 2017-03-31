2a0b7944b5c336acb6f427f3a8b587487ce26d74
Ron <ronll64@gmail.com>
2017-03-31 11:26:26 +0300

    modified bit_serializer.h

--- bit_serializer.h
+++ bit_serializer.h
@@ -6,13 +6,13 @@
  0 - success
  1 - unsucces
  */
-int bits2bytes(__IN__ const char* bits, __IN__ unsigned int bits_length, __OUT__ unsigned char* bytes, __OUT__ unsigned int* bytes_length);
+int bits2bytes(__IN__ const unsigned char* bits, __IN__ unsigned int bits_length, __OUT__ unsigned char* bytes, __OUT__ unsigned int* bytes_length);
 
-int bytes2bits(__IN__ const char* bytes, __IN__ unsigned int bytes_length, __OUT__ unsigned char* bits, __OUT__ unsigned int* bits_length);
+int bytes2bits(__IN__ const unsigned char* bytes, __IN__ unsigned int bytes_length, __OUT__ unsigned char* bits, __OUT__ unsigned int* bits_length);
 
 /**
  return codes:
  0,1 - the value of the bit
  -1 - error
  */
-char get_bit_in_byte_array(__IN__ const char bytes, __IN__ unsigned int bit_index);
+char get_bit_in_byte_array(__IN__ const unsigned char* bytes, __IN__ unsigned int bit_index);
