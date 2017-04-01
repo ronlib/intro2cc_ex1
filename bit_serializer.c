7208b9db6c992b4bcd3308b6afa0b6a3b5b1d688
Ron <ronll64@gmail.com>
2017-04-01 16:49:21 +0300

    Removed needless comments

--- bit_serializer.c
+++ bit_serializer.c
@@ -2,10 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* int bits2bytes(__IN__ const char* bits, __IN__ unsigned int bits_length, __OUT__ unsigned char* bytes, __OUT__ unsigned int* bytes_length) */
-/* { */
-
-/* } */
 
 char get_bit_in_byte_array(__IN__ const unsigned char* bytes, __IN__ unsigned int bit_index)
 {
